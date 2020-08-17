ac739da26744db6cf037c302749056ab5cdd00a5
Oskar <saturdaysheroes.business@gmail.com>
2020-08-17 15:43:50 +0200

    added different twitter package

--- src/twitter.js
+++ src/twitter.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv"; dotenv.config();
-import Twitter from "twitter";
+import Twit from "twit";
 
-const Client = new Twitter({
+const Client = new Twit({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
