70ffe9ccd574b94bb93c8b2e6da4ebe85493adfd
Oskar <saturdaysheroes.business@gmail.com>
2020-08-17 17:09:53 +0200

    added shouldReply bool

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import {Twitter} from "./twitter.js";
 // TVP Info, TVN 24, Polsat News, Onet, Rzeczpospolita, NewsweekPolska. 
 const profiles = [47295451, 14493047, 2383788666, 15720598, 194399035, 53054655];
 const message = " Chciałbyś automatycznie otrzymywać wiadomości ze wszystkich polskich mediów? Daj mi followa! Jestem w pełni niezależnym botem retweetującym wszystkie polskie media."
+const shouldReply = true; 
 
 const stream = Twitter.stream("statuses/filter", {follow: profiles}); 
 stream.on("tweet", (tweet) => {
     if (!profiles.includes(tweet.user.id)) return;
 
-    Twitter.post("statuses/update", {status: "@" + tweet.user.screen_name + message, in_reply_to_status_id_str: tweet.id_str}, (err) => {
-        if (err) throw err;
+    if (shouldReply) {
+        Twitter.post("statuses/update", {status: `@${tweet.user.screen_name} ${message}`, in_reply_to_status_id: tweet.id_str, auto_populate_reply_metadata: true}, (err) => {
+            if (err) throw err;
 
-        console.log("[REPLY] Successfully replied!")
-    });
+            console.log("[REPLY] Successfully replied!")
+        });
+    };
 
     Twitter.post("statuses/retweet/" + tweet.id_str, {}, (err) => {
         if (err) throw err;
