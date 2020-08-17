7189b405ccb523e1960bed7b69da49146c5e9a3f
Oskar <saturdaysheroes.business@gmail.com>
2020-08-17 15:51:45 +0200

    added retweet functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import {Twitter} from "./twitter.js";
 
 // Profile IDs to follow.
-// TVP Info, TVN 24, Polsat News. 
-const profiles = [47295451, 14493047, 2383788666];
+// TVP Info, TVN 24, Polsat News, Onet. 
+const profiles = [47295451, 14493047, 2383788666, 15720598];
 
 const stream = Twitter.stream("statuses/filter", {follow: profiles}); 
 stream.on("tweet", (tweet) => {
+    if (!profiles.includes(tweet.user.id)) return;
+
     console.log(tweet);
+
+    Twitter.post("statuses/retweet/:id", {id: tweet.id}, (err, data, res) => {
+        if (err) throw err;
+
+        console.log(data);
+    });
 });
