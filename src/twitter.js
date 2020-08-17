db2de6d8b7250bf20d40fe776a51d67172a128e9
Oskar <saturdaysheroes.business@gmail.com>
2020-08-17 14:33:19 +0200

    added twitter client

---
+++ src/twitter.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv"; dotenv.config();
+import Twitter from "twitter";
+
+const Client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+export {Client as Twitter};
