6b3228686397b0636d17cede77b19e281f11c328
Oskar <saturdaysheroes.business@gmail.com>
2020-08-17 16:24:30 +0200

    added promotional message

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import {Twitter} from "./twitter.js";
 // Profile IDs to follow.
 // TVP Info, TVN 24, Polsat News, Onet, Rzeczpospolita, NewsweekPolska. 
 const profiles = [47295451, 14493047, 2383788666, 15720598, 194399035, 53054655];
+const message = "Chciałbyś automatycznie otrzymywać wiadomości ze wszystkich polskich mediów? Daj mi followa! Jestem w pełni niezależnym botem retweetującym wszystkie polskie media."
 
 const stream = Twitter.stream("statuses/filter", {follow: profiles}); 
 stream.on("tweet", (tweet) => {
     if (!profiles.includes(tweet.user.id)) return;
 
-    Twitter.post("statuses/update", {status: "Chciałbyś automatycznie otrzymywać wiadomości ze wszystkich polskich mediów? Daj mi followa już dziś!", in_reply_to_status_id: tweet.id_str}, (err) => {
+    Twitter.post("statuses/update", {status: message, in_reply_to_status_id: tweet.id_str}, (err) => {
         if (err) throw err;
 
         console.log("[REPLY] Successfully replied!")
