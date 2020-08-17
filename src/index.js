582ce70a1303b3b406164e6146fff519a8935d73
Oskar <saturdaysheroes.business@gmail.com>
2020-08-17 15:43:58 +0200

    created sample stream

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import {Twitter} from "twitter.js";
+import {Twitter} from "./twitter.js";
+
+// Profile IDs to follow.
+// TVP Info, TVN 24, Polsat News. 
+const profiles = [47295451, 14493047, 2383788666];
+
+const stream = Twitter.stream("statuses/filter", {follow: profiles}); 
+stream.on("tweet", (tweet) => {
+    console.log(tweet);
+});
