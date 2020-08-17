20238891e233a7740fc8c452b2f6bfb4fca363c6
Oskar <saturdaysheroes.business@gmail.com>
2020-08-17 17:48:02 +0200

    added const to represent cooldown

--- src/index.js
+++ src/index.js
@@ -13,13 +13,14 @@ const profiles = [
 ];
 const message = " Chciałbyś automatycznie otrzymywać wiadomości ze wszystkich polskich mediów? Daj mi followa! Jestem w pełni niezależnym botem retweetującym wszystkie polskie media."
 const shouldReply = true; 
+const cooldown = 180 * 1000
 var lastReply = Date.now();
 
 const stream = Twitter.stream("statuses/filter", {follow: profiles}); 
 stream.on("tweet", (tweet) => {
     if (!profiles.includes(tweet.user.id)) return;
 
-    if (shouldReply && (Date.now() - lastReply > 300)) {
+    if (shouldReply && (Date.now() - lastReply > cooldown)) {
         Twitter.post("statuses/update", {status: `@${tweet.user.screen_name} ${message}`, in_reply_to_status_id: tweet.id_str, auto_populate_reply_metadata: true}, (err) => {
             if (err) throw err;
 
