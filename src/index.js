5d2af96aebd631b65dde01425981ea6a2af48e4b
Oskar <saturdaysheroes.business@gmail.com>
2020-08-19 15:35:00 +0200

    removed redundant code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import {Twitter} from "./twitter.js";
 
 const message = " Chciałbyś automatycznie otrzymywać wiadomości ze wszystkich polskich mediów? Daj mi followa! Jestem w pełni niezależnym botem retweetującym wszystkie polskie media."
-const shouldReply = true; 
-const cooldown = 60 * 1000;
 var lastReply = Date.now();
 
+// Check all of the accounts that we follow, then create a stream with them. 
+// This will save us time, we won't have to hard-code IDs. 
 Twitter.get("friends/ids", {screen_name: "media_polskie"}, (err, data) => {
     if (err) {
         console.log("[ERROR] " + err);
@@ -16,7 +16,7 @@ Twitter.get("friends/ids", {screen_name: "media_polskie"}, (err, data) => {
     stream.on("tweet", (tweet) => {
         if (!profiles.includes(tweet.user.id)) return;
 
-        if (shouldReply && (Date.now() - lastReply > cooldown)) {
+        if (Date.now() - lastReply > (60 * 1000)) {
             Twitter.post("statuses/update", {status: `@${tweet.user.screen_name} ${message}`, in_reply_to_status_id: tweet.id_str, auto_populate_reply_metadata: true}, (err) => {
                 if (err) {
                     console.log("[ERROR] " + err);
