3eac269ad6e225dc99cbccbcf8e80d72500ccacd
Oskar <saturdaysheroes.business@gmail.com>
2020-08-19 15:19:00 +0200

    used following instead of hard-coded array

--- src/index.js
+++ src/index.js
@@ -1,45 +1,40 @@
 import {Twitter} from "./twitter.js";
 
-// Profile IDs to follow.
-const profiles = [
-    47295451, // TVP Info 
-    14493047, // TVN 24
-    2383788666, // Polsat News
-    15720598, // Onet
-    194399035, // Rzeczpospolita
-    53054655, // Newsweek Polska
-    1073461458, // Telewizja Republika
-    153802461, // wPolityce
-    1076576528, // DoRzeczy
-    19179390 // Wyborcza
-];
 const message = " Chciałbyś automatycznie otrzymywać wiadomości ze wszystkich polskich mediów? Daj mi followa! Jestem w pełni niezależnym botem retweetującym wszystkie polskie media."
 const shouldReply = true; 
-const cooldown = 180 * 1000
+const cooldown = 60 * 1000;
 var lastReply = Date.now();
 
-const stream = Twitter.stream("statuses/filter", {follow: profiles}); 
-stream.on("tweet", (tweet) => {
-    if (!profiles.includes(tweet.user.id)) return;
+Twitter.get("friends/ids", {screen_name: "media_polskie"}, (err, data) => {
+    if (err) {
+        console.log("[ERROR] " + err);
+        return;
+    }; 
 
-    if (shouldReply && (Date.now() - lastReply > cooldown)) {
-        Twitter.post("statuses/update", {status: `@${tweet.user.screen_name} ${message}`, in_reply_to_status_id: tweet.id_str, auto_populate_reply_metadata: true}, (err) => {
+    const profiles = data.ids
+    const stream = Twitter.stream("statuses/filter", {follow: profiles}); 
+    stream.on("tweet", (tweet) => {
+        if (!profiles.includes(tweet.user.id)) return;
+
+        if (shouldReply && (Date.now() - lastReply > cooldown)) {
+            Twitter.post("statuses/update", {status: `@${tweet.user.screen_name} ${message}`, in_reply_to_status_id: tweet.id_str, auto_populate_reply_metadata: true}, (err) => {
+                if (err) {
+                    console.log("[ERROR] " + err);
+                    return;
+                };
+
+                console.log("[REPLY] Successfully replied!")
+                lastReply = Date.now();
+            });
+        };
+
+        Twitter.post("statuses/retweet/" + tweet.id_str, {}, (err) => {
             if (err) {
                 console.log("[ERROR] " + err);
                 return;
             };
 
-            console.log("[REPLY] Successfully replied!")
-            lastReply = Date.now();
+            console.log("[RETWEET] Successfully retweeted!");
         });
-    };
-
-    Twitter.post("statuses/retweet/" + tweet.id_str, {}, (err) => {
-        if (err) {
-            console.log("[ERROR] " + err);
-            return;
-        };
-
-        console.log("[RETWEET] Successfully retweeted!");
     });
 });
